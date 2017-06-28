bee2c35e396051829f2161a2966352803d04e35e
AparnaChinya <apchin@microsoft.com>
2017-06-29 01:43:43 +0530

    TwoSum

---
+++ TwoSum.cpp
@@ -0,0 +1,31 @@
+/*Q: https://leetcode.com/problems/two-sum/#/description
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    unordered_map<int,int> myMap;
+    
+    int n = nums.size();
+    
+    for(int i=0;i<n;i++)
+    {
+        auto p = myMap.find(target-nums[i]);
+        if(p!=myMap.end())
+        {
+            return {p->second,i};
+        }
+        myMap[nums[i]]=i;
+    }
+        
+    }
+};
