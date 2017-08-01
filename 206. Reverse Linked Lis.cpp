43458b433d55d2a7c0185e85039435228b588068
AparnaChinya <apchin@microsoft.com>
2017-08-01 12:52:16 +0530

    Day13

---
+++ 206. Reverse Linked Lis.cpp
@@ -0,0 +1,30 @@
+/*
+Reverse a singly linked list.
+
+From <https://leetcode.com/problems/reverse-linked-list/tabs/description> 
+
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL) return head;
+        
+        ListNode *p = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+        return p;
+        
+    }
+};
+
