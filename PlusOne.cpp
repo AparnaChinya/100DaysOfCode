07dce511490cc9e938ac7bdeea5e0c3e5f032bab
AparnaChinya <cr.aparna@gmail.com>
2018-07-21 21:49:56 -0700

    Day2

---
+++ PlusOne.cpp
@@ -0,0 +1,34 @@
+/* Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+Example 1:
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+
+From <https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1148/>  */
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0 ; i--) {
+            if(digits[i] < 9) {
+                digits[i] += 1;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        if(digits[0] == 0) {
+            digits.insert(digits.begin(), 1);
+        }
+
+        return digits;
+    }
+};
+
